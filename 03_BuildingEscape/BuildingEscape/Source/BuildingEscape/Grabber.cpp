6bdf01d84476c26cff0cadb227e595910856dc3a
Filip Krstevski WORK <fkrstevski@gmail.com>
2017-08-31 11:04:42 -0400

    Adding debug trace for the player view

--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
-
+#include "Runtime/Engine/Classes/Engine/World.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -18,9 +19,7 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
 	UE_LOG(LogTemp, Warning, TEXT("Grabbr reporting for duty!!!"));
-	
 }
 
 
@@ -29,6 +28,32 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player view point location and rotation
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		PlayerViewPointLocation, 
+		PlayerViewPointRotation
+	);
+
+	/*
+	UE_LOG(LogTemp, Warning, TEXT("Player Location: %s Rotation: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString());
+		*/
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		-1,
+		0,
+		10
+	);
+
 }
 
